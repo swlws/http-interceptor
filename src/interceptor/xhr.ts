1a8e3ad6124c4a2fe6c584bfc8597fe844d0a039
swlws <sunwenlong03@ky-tech.com>
2024-06-27 11:33:17 +0800

    xhr 兼容

--- src/interceptor/xhr.ts
+++ src/interceptor/xhr.ts
@@ -16,7 +16,12 @@ export default class XhrInterceptor<
 
     const NativeXhr = window.XMLHttpRequest;
     class CustomXhr extends NativeXhr {
+      private _method!: string;
       private _src = '' as K;
+      private _async!: boolean;
+      private _username?: string | null;
+      private _password?: string | null;
+      private _headers: Record<string, string> = {};
 
       open(
         method: string,
@@ -25,17 +30,39 @@ export default class XhrInterceptor<
         username?: string | null,
         password?: string | null
       ) {
+        this._method = method;
         this._src = url;
+        this._async = async;
+        this._username = username;
+        this._password = password;
+      }
 
-        super.open(method, url, async, username, password);
+      setRequestHeader(name: string, value: string) {
+        this._headers[name] = value;
       }
 
       send(body?: T) {
-        if (_this.useNative) {
-          return super.send(body);
+        let url = '' as K;
+        let data = body;
+
+        if (!_this.useNative) {
+          [url, data] = _this.callFilterAndAdapter(this._src, body);
         }
 
-        const [url, data] = _this.callFilterAndAdapter(this._src, body);
+        // Open
+        super.open(
+          this._method,
+          url,
+          this._async,
+          this._username,
+          this._password
+        );
+
+        // 设置请请求头
+        Object.keys(this._headers).forEach((key) => {
+          this.setRequestHeader(key, this._headers[key]);
+        });
+
         return super.send(data);
       }
     }
